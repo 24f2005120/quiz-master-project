bc09eeff9ca2b760338d0a08c9cc5d888829ef49
R0b0ticRabb1t <anirudha.py@gmail.com>
2025-03-06 00:10:05 +0400

    database stuff, made the models, configured relations (haven't tested anything cascade may not work)

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, requests
+from flask import Flask, render_template, request
+
+import models
+from database import db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-app.route("/", methods=["GET"])
 
-
+@app.route("/", methods=["GET"])
 def home():
-    return "<h1>Hello World<\\h1>"
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ models.py
@@ -0,0 +1,75 @@
+from datetime import date, time
+from typing import Annotated, List, Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database import db
+
+# handy custom types
+id_pk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
+
+
+class Subject(db.Model):
+    subject_id: Mapped[id_pk]
+    subject_name: Mapped[str]
+    description: Mapped[str]
+
+    chapters: Mapped[List["Chapter"]] = relationship(
+        back_populates="chapter", cascade="all,delete-orphan"
+    )
+
+
+class Chapter(db.Model):
+    subject_id: Mapped[int] = mapped_column(ForeignKey("subject.subject_id"))
+    chapter_id: Mapped[id_pk]
+    chapter_name: Mapped[str]
+    description: Mapped[str]
+
+    subject: Mapped["Subject"] = relationship(back_populates="chapters")
+    quizzes: Mapped[List["Quiz"]] = relationship(back_populates="chapter")
+
+
+class Quiz(db.Model):
+    chapter_id: Mapped[Optional[int]] = mapped_column(ForeignKey("chapter.chapter_id"))
+    quiz_id: Mapped[id_pk]
+    quiz_date: Mapped[Optional[date]]
+    quiz_duration: Mapped[Optional[int]]  # minutes? hopefully
+    remarks: Mapped[str]
+
+    chapter: Mapped["Chapter"] = relationship(back_populates="quizzes")
+    questions: Mapped[List["Question"]] = relationship(
+        back_populates="quiz", cascade="all,delete-orphan"
+    )
+    scores: Mapped[List["Score"]] = relationship(
+        back_populates="quiz", cascade="all,delete-orphan"
+    )
+
+
+class Question(db.Model):
+    quiz_id: Mapped[int] = mapped_column(ForeignKey("quiz.quiz_id"))
+    question_id: Mapped[id_pk]
+    text: Mapped[str]
+    marks: Mapped[int]
+
+    quiz: Mapped["Quiz"] = relationship(back_populates="questions")
+
+
+class Score(db.Model):
+    quiz_id: Mapped[int] = mapped_column(ForeignKey("quiz.quiz_id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.user_id"), primary_key=True)
+    start_time: Mapped[time]
+    time_taken: Mapped[int]  # we'll be storing as seconds
+
+    quiz: Mapped["Quiz"] = relationship(back_populates="scores")
+    user: Mapped["User"] = relationship(back_populates="scores")
+
+
+class User(db.Model):
+    user_id: Mapped[id_pk]
+    user_name: Mapped[str]
+    password: Mapped[str]
+
+    scores: Mapped[List["Score"]] = relationship(
+        back_populates="user", cascade="all,delete-orphan"
+    )
