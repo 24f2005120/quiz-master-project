a76c7dcd2717f189585536eb4770bafc75ab99e1
R0b0ticRabb1t <anirudha.py@gmail.com>
2025-03-13 00:49:41 +0400

    added user signup functionality
    no actual site yet though i'll do that next hm

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -9,6 +9,10 @@ session = db.session
 auth_bp = Blueprint("auth", __name__)
 
 
+def select_user(username):
+    return session.scalar(select(User).where(User.username == username))
+
+
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -17,15 +21,17 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        user = session.scalar(select(User).where(User.username == username))
+        user = select_user(username)
         if user:
             login_user(user)
             if user.password != password:
+                print("incorrect password error ")
                 return redirect("/login")
             if user.username == "admin":
                 return redirect("/admin")
             return redirect("/user")
         else:
+            print("user not found error")
             return redirect("/login")
 
 
@@ -34,4 +40,15 @@ def signup():
     if request.method == "GET":
         return render_template("signup.html")
     if request.method == "POST":
-        return
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if select_user(username):
+            print("user already exists error")
+            return redirect("/signup")
+
+        user = User(username=username, password=password)
+        session.add(user)
+        session.commit()
+        login_user(user)
+        return redirect("/user")
