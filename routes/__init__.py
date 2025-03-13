59b5a7973128d7e0be98cf608e5efbbaf21169ba
R0b0ticRabb1t <anirudha.py@gmail.com>
2025-03-13 19:23:05 +0400

    added an index page to choose between login and signup
    and also changed the routes in html files to be dynamic with url_for()

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,11 +5,13 @@ from models import User, db
 
 from .admin_routes import admin_bp
 from .auth_routes import auth_bp
+from .main_routes import main_bp
 
 
 def init_routes(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
+    app.register_blueprint(main_bp)
 
 
 def init_login_manager(app):
--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, redirect, request
+from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import current_user
 
 main_bp = Blueprint("main", __name__)
 
-@main_bp.route("/", methods = ["GET", "POST"])
-def home():
+
+@main_bp.route("/", methods=["GET", "POST"])
+def index():
     if current_user.is_authenticated:
         return redirect("/redirect")
-    if request.method
-    if request.method=="GET":
-
+    if request.method == "GET":
+        return render_template("index.html")
